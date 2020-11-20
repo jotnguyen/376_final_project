5f73abfc2aba647edab7d71fc8d73530dc1360d8
John Nguyen <johntnguyen9@gmail.com>
2020-11-20 13:21:30 -0600

    Add moving cursor

---
+++ Assets/GunScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    private Vector3 zAngle;
+    private float angle = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        mousePos.z = 0f;
+
+        // Gun following mouse
+        // Set ray length to 1
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        // How much of an angle between positive x axis and ray to point x y
+        angle = (Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg) - 90;
+        zAngle = new Vector3(0, 0, angle);
+        // 3 angles that describe orientation of rigid body
+        transform.eulerAngles = zAngle;
+    }
+}
---
+++ Assets/ManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerScript : MonoBehaviour
+{
+
+    public GameObject cursor;
+    private Vector3 mouseVector;
+    private Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseVector = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+        // wherever the mouse is at set it up
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(mouseVector);
+        cursor.transform.position = new Vector2(target.x, target.y); 
+    }
+}
