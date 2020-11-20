99bb3d049938839f469463c19ec0faa2ea10ab2d
John Nguyen <johntnguyen9@gmail.com>
2020-11-20 12:49:18 -0600

    Moving blue square player added

---
+++ Assets/Scenes/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    Rigidbody2D rb;
+    private float speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+}
