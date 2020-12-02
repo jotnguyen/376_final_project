b5ece934d5dadc50cc9af8340cf060372eee0481
John Nguyen <johntnguyen9@gmail.com>
2020-12-02 13:13:26 -0600

    Enemy moves to player now

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 5;
+    int MinDist = 2;
+
+
+
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Player.position, MoveSpeed * Time.deltaTime);
+
+    }
+}
